0101593ec743ca80afc1ac8c9c955e9a72b7fdc9
Danilo Maia <isaqquemaiia7@outlook.com.br>
2023-02-26 17:58:16 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -58,7 +58,26 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller obj) {
+        try (
+                PreparedStatement st = conn.prepareStatement("""
+            UPDATE seller
+            SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?
+            WHERE
+            seller.Id = ?
+            """,
+                        Statement.RETURN_GENERATED_KEYS)
+        ) {
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, Date.valueOf(obj.getBirthDate()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
 
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
